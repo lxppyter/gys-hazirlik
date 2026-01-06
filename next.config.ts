bc814e0da1b24570a031fc7c8ac42c05a3294304
jxpyter <149976561+jxpyter@users.noreply.github.com>
2026-01-06 08:46:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // No special output config needed for Netlify with plugin
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
